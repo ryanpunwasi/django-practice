4b9a5a8dcff9b7d73e0a1a411143de6c9f08748e
Ryan Punwasi <ryan.punwasi@gmail.com>
2024-08-19 23:42:27 -0400

    Use templates

--- src/testproject/views.py
+++ src/testproject/views.py
@@ -1,4 +1,28 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def home_page_view(request, *args, **kwargs):
-    return HttpResponse('<h1>Hello, World!</h1>')
+    title = "Hello World"
+    my_context = {
+        "page_title": title
+    }
+    return HttpResponse(render(request, "home.html", my_context))
+
+def old_home_page_view(request, *args, **kwargs):
+    title = "Hello World"
+    my_context = {
+        "page_title": title
+    }
+    return HttpResponse("""<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <title>{page_title}</title>
+  </head>
+
+  <body>
+    <h1>{page_title}</h1>
+    <p>Welcome to the {page_title} page.</p>
+  </body>
+</html>
+""".format(**my_context))
